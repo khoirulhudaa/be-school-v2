c8b4dfada3aa6bec3c10237093a7b0937c583119
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-04-20 09:22:41 +0700

    gix

--- routes/index.js
+++ routes/index.js
@@ -89,7 +89,7 @@
     router.use('/export-excel', exportExcel);
     router.use('/sekolah', dataSekolah);
     router.use('/tugas', tugasSekolah);
-    router.use('/konselling', konselling);
+    router.use('/konselling', konsellingRoutes);
     router.use('/scan-qr', scanQrStatis);
     router.use('/orang-tua', orangTua);
     router.use('/face', faceRouter);
